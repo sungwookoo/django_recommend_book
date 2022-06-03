b7244761fcf5584b692e58dae5319100ea4db302
gyuhyeongH <ckacl792@gmail.com>
2022-06-03 10:14:51 +0900

    feat : 로그인 기능, 팔로우기능, 회원가입 기능, admin에 usermodel추가,

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.sign_up_view, name='sign-up'),
+    path('sign-up/', views.sign_up_view, name='sign-up'),
+    path('sign-in/', views.sign_in_view, name='sign-in'),
+    path('logout/', views.logout, name='logout'),
+    path('user/follow/<int:id>/', views.user_follow, name='user-follow'),
 
 ]
+
+
