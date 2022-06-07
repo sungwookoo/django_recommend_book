d5bb3557c20bf970799b6fbb730000899d7da87a
gyuhyeongH <ckacl792@gmail.com>
2022-06-07 16:55:40 +0900

    feat : book 모델에 저장기능 추가

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 # user/views.py
 from django.shortcuts import render, redirect
 from .models import UserModel
+from book.models import Book
 from django.http import HttpResponse
 # user/views.py
 from django.contrib.auth import get_user_model  # 사용자가 있는지 검사하는 함수
@@ -11,19 +12,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def sign_up_view(request):
+    bestseller = crawling_bestseller()
     if request.method == 'GET':
         user = request.user.is_authenticated  # 로그인 된 사용자가 요청하는지 검사
         if user:  # 로그인이 되어있다면
             return redirect('/')
         else:  # 로그인이 되어있지 않다면
-            bestseller = crawling_bestseller()
-            print(bestseller)
             return render(request, 'signup.html', {'bestseller':bestseller})
     elif request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         password2 = request.POST.get('password2', '')
-
+        my_book = request.POST.get('book', '')
+        my_title=bestseller[int(my_book)-1]['title']
+        my_img=bestseller[int(my_book)-1]['img']
+        print(my_title)
         if password != password2:
             # 패스워드가 다르다는 에러가 필요합니다. {'error':'에러문구'} 를 만들어서 전달합니다.
             return render(request, 'signup.html', {'error': '패스워드를 확인 해 주세요!'})
@@ -38,6 +41,11 @@ def sign_up_view(request):
                               {'error': '사용자가 존재합니다.'})  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
             else:
                 UserModel.objects.create_user(username=username, password=password)
+                Book.title = my_title
+                Book.img_url = my_img
+                Book.like_users = username
+                Book.save()
+
                 return redirect('/sign-in')  # 회원가입이 완료되었으므로 로그인 페이지로 이동
 
 
@@ -93,8 +101,9 @@ def crawling_bestseller():
     soup = BeautifulSoup(data.text, 'html.parser')
     books = soup.select('#main_contents > ul > li')
     bestseller=[]
+    c=0
     for book in books:
-        # movie 안에 a 가 있으면,
+        c +=1
         best_image = book.select_one('div.cover > a > img')
-        bestseller.append({'title':best_image['alt'], 'img':best_image['src']})
+        bestseller.append({'rank':c ,'title':best_image['alt'], 'img':best_image['src']})
     return bestseller
