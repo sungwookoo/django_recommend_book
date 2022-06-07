05d06c698eeebc1008335cdda207659c48592cc9
sungwookoo <sungwookoo.dev@gmail.com>
2022-06-07 16:50:33 +0900

    feat : 검색기능 추가 및 insert_book_data 메서드 작성

--- book/views.py
+++ book/views.py
@@ -1,9 +1,16 @@
+import gensim.models
 from django.contrib import messages
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from django.shortcuts import render, redirect
-
+import MeCab
+import pandas as pd
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 # Create your views here.
+from book.models import BookData
+
+
 def home(request):
     # user = request.user.is_authenticated
     # if user:
@@ -15,11 +22,50 @@ def home(request):
 
 def get_book(request):
     if request.method == 'GET':
-        book_list = []
-        for i in range(1, 100001):
-            book_list.append(
-                {'title': '제목' + str(i), 'author': '저자' + str(i), 'publishar': '출판사' + str(i), 'desc': '내용' + str(i)})
+        # book_list = []
+        # for i in range(1, 100001):
+        #     book_list.append(
+        #         {'title': '제목' + str(i), 'author': '저자' + str(i), 'publisher': '출판사' + str(i), 'desc': '내용' + str(i)}
+        #     )
 
+        # df = pd.read_table('book/doc2vec/book.csv', sep=',')
+        #
+        # for index in range(0, len(df['title'])):
+        #     book_list.append(
+        #         {'master_seq': df['master_seq'][index], 'title': df['title'][index], 'img': df['img_url'][index],
+        #          'description': df['description'][index], 'author': df['author'][index], 'price': df['price'][index],
+        #          'pub_date': df['pub_date_2'][index],
+        #          'publisher': df['publisher'][index]})
+
+        # df = pd.read_table('book/doc2vec/book.csv', sep=',')
+        # mecab = MeCab.Tagger()
+        # df['token'] = 0
+        # for i in range(0, len(df['title'])):
+        #     tmp = mecab.parse(df['title'][i]).split()
+        #     # tmp2 = mecab.parse(str(df['description'][i])).split()
+        #
+        #     # tmp3 = tmp + tmp2
+        #
+        #     tokens = []
+        #     for k in range(0, len(tmp) - 2, 2):
+        #         tokens.append(tmp[k])
+        #     df['token'][i] = tokens
+        # documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['token'])]
+        # model = Doc2Vec(documents, vector_size=100, window=3, epochs=1, min_count=3, workers=4)
+        # model = gensim.models.Doc2Vec.load('book/doc2vec/model.doc2vec')
+        # inferred_doc_vec = model.infer_vector(df['token'])
+        # print(inferred_doc_vec)
+        # most_similar_docs = model.docvecs.most_similar([inferred_doc_vec], topn=10)
+        # print(df['title'][100])
+        # book_list = []
+        # for index, similarity in most_similar_docs:
+        #     book_list.append(
+        #         {'master_seq': df['master_seq'][index], 'title': df['title'][index], 'img': df['img_url'][index],
+        #          'description': df['description'][index], 'author': df['author'][index], 'price': df['price'][index],
+        #          'pub_date': df['pub_date_2'][index],
+        #          'publisher': df['publisher'][index]})
+
+        book_list = BookData.objects.all()
         search_text = request.GET.get('search_text', '')
         page = request.GET.get('page', 1)
 
@@ -28,10 +74,15 @@ def get_book(request):
                 messages.warning(request, "검색어는 최소 2자 이상이여야 합니다.")
                 return redirect('/book')
 
-            result_list = []
-            for book in book_list:
-                if search_text in book['author'] or search_text in book['title'] or search_text in book['publishar']:
-                    result_list.append(book)
+            result_list = book_list.filter(
+                Q(title__icontains=search_text) |
+                Q(author__icontains=search_text) |
+                Q(publisher__icontains=search_text)
+            )
+
+            # for book in book_list:
+            #     if search_text in book['author'] or search_text in book['title'] or search_text in book['publisher']:
+            #         result_list.append(book)
 
             paginator = Paginator(result_list, 20)
             try:
@@ -53,7 +104,23 @@ def get_book(request):
                 book_list = paginator.page(paginator.num_pages)
             return render(request, 'home.html', {'all_book': book_list})
 
-def detail_book(request,id):
+
+def detail_book(request, id):
     # book = (bookDB).objects.get(id=id)
-    book =  {'title': '제목' + str(id), 'author': '저자' + str(id), 'publishar': '출판사' + str(id), 'desc': '내용' + str(id)}
-    return render(request,'detail.html', {'book': book})
+    book = {'title': '제목' + str(id), 'author': '저자' + str(id), 'publisher': '출판사' + str(id), 'desc': '내용' + str(id)}
+    return render(request, 'detail.html', {'book': book})
+
+
+def insert_book_data(request):
+    df = pd.read_table('book/doc2vec/book.csv', sep=',')
+    for index in range(0, len(df['title'])):
+        book_data = BookData()
+        book_data.master_seq = df['master_seq'][index]
+        book_data.title = df['title'][index]
+        book_data.img_url = df['img_url'][index]
+        book_data.description = df['description'][index]
+        book_data.author = df['author'][index]
+        book_data.price = df['price'][index]
+        book_data.pub_date = df['pub_date_2'][index]
+        book_data.publisher = df['publisher'][index]
+        book_data.save()
