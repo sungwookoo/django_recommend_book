488f364f7371bee1e5d6e3ea5febf4239417f963
gyuhyeongH <ckacl792@gmail.com>
2022-06-09 15:09:56 +0900

    feat : 크롤링 데이터 저장 기능 추가

--- book/urls.py
+++ book/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('book/', views.get_book, name='book'),
     path('book/<int:id>', views.detail_book, name='detail_book'),
     path('api/book_data', views.insert_book_data, name='insert_book_data'),
+    path('api/crawling_data', views.insert_crawling_data, name='insert_crawling_data'),
     path('book/review/<int:id>',views.write_review,name='write_review'),
     path('book/review/delete/<int:id>',views.delete_review,name='delete_review'),
     path('book/review/edit/<int:id>',views.edit_review,name='edit_review'),
