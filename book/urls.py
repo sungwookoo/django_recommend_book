a10e579d388f5f5afb3fc4af44365fc475fc3155
sungwookoo <sungwookoo.dev@gmail.com>
2022-06-10 09:01:46 +0900

    chore : 로딩 url 추가

--- book/urls.py
+++ book/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('book/', views.get_book, name='book'),
+    path('', views.loading, name='loading'),
+    path('api/loading_proc', views.loading_proc, name='loading_proc'),
+    path('home/', views.home, name='home'),
+    path('book/', views.get_book, name='get_book'),
+    path('book/recommend/<int:id>', views.get_recommend_list, name='get_recommend_list'),
     path('book/<int:id>', views.detail_book, name='detail_book'),
     path('api/book_data', views.insert_book_data, name='insert_book_data'),
     path('api/crawling_data', views.insert_crawling_data, name='insert_crawling_data'),
-    path('book/review/<int:id>',views.write_review,name='write_review'),
-    path('book/review/delete/<int:id>',views.delete_review,name='delete_review'),
-    path('book/review/edit/<int:id>',views.edit_review,name='edit_review'),
-    path('book/review/edit/update/<int:id>',views.update,name='update'),
-    path('book/likes/<int:book_id>',views.likes,name='likes'),
+    path('book/review/<int:id>', views.write_review, name='write_review'),
+    path('book/review/delete/<int:id>', views.delete_review, name='delete_review'),
+    path('book/review/edit/<int:id>', views.edit_review, name='edit_review'),
+    path('book/review/edit/update/<int:id>', views.update, name='update'),
+    path('book/likes/<int:book_id>', views.likes, name='likes'),
 ]
